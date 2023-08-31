a70e0972a82b1f10b9e4e0c079493de3686dff71
Lorenzo Del Gaudio <lorenzo.delgaudio@datavision.cz>
2023-08-31 09:46:42 +0200

    Add animation for cover image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'fdk-lombardia-frontend';
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    const imageContainer = document.querySelector('.image-container');
+    const coverImage = new Image();
+    coverImage.src = "../assets/src/images/copertina.webp";
+
+    coverImage.onload = () => {
+      imageContainer?.classList.add('loaded');
+    };
+
+  }
 }
